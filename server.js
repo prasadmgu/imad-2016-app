fd84edd0ae877f6add736a452fc7cb3b887cc52c
prasadmgu <prasadmgu@gmail.com>
2016-10-08 11:47:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName',function(req, res){
     var articleName=req.params.articleName;
-res.send(createTemplate(articles[aticleName]));   
+res.send(createTemplate(articles[articleName]));   
 });
 app.get('/article-two',function(req, res){
 res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
